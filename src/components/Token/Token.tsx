d8289cc05ff351022b293fbcf62e8dd669afea40
Silvio Paganini <paganini.silvio@bcgplatinion.com>
2021-03-08 10:44:25 +0000

    theme fix

--- src/components/Token/Token.tsx
+++ src/components/Token/Token.tsx
@@ -46,113 +46,121 @@ const Token = ({ token, isOnSale, onTransfer, onBuy, onSale }: TokenCompProps) =
   return (
     <Card variant="nft">
       <Image
-        sx={{ width: '100%', mb: 1, border: '1px solid black', bg: 'white' }}
+        sx={{ width: '100%', bg: 'white', borderBottom: '1px solid black' }}
         src={`https://robohash.org/${token.uri}.png`}
       />
-      <Heading as="h2">{token.uri}</Heading>
-      <Divider variant="divider.nft" />
-      <Box>
-        <Heading as="h3" sx={{ color: 'green', m: 0 }}>
-          Ξ {Number(Web3.utils.fromWei(token.price)).toFixed(2)}
-        </Heading>
-        <Text sx={{ fontSize: 1 }}>Price listing</Text>
+      <Box p={3} pt={2}>
+        <Heading as="h2">{token.uri}</Heading>
+        <Divider variant="divider.nft" />
+        <Box>
+          <Heading as="h3" sx={{ color: 'green', m: 0, fontWeight: 'bold' }}>
+            Ξ {Number(Web3.utils.fromWei(token.price)).toFixed(2)}
+          </Heading>
+          <Text sx={{ color: 'midGray', fontSize: 1 }}>Price listing</Text>
+        </Box>
+
+        {onTransfer && (
+          <Flex mt={3} sx={{ justifyContent: 'center' }}>
+            {transfer && (
+              <Box sx={{ width: '100%' }}>
+                <Flex
+                  onSubmit={onTransferClick}
+                  sx={{ width: '100%', flexDirection: 'column' }}
+                  as="form"
+                >
+                  <Input
+                    onChange={e => setAddress(e.currentTarget.value)}
+                    placeholder="ETH Address 0x0..."
+                  />
+                </Flex>
+                <Flex mt={2}>
+                  <Button sx={{ bg: 'green' }} onClick={onTransferClick} variant="quartiary">
+                    Confirm
+                  </Button>
+                  <Button
+                    sx={{ bg: 'red' }}
+                    ml={2}
+                    onClick={() => setTransfer(false)}
+                    variant="quartiary"
+                  >
+                    Cancel
+                  </Button>
+                </Flex>
+              </Box>
+            )}
+            {onSaleActive && (
+              <Box sx={{ width: '100%' }}>
+                <Flex
+                  onSubmit={onTransferClick}
+                  sx={{ width: '100%', flexDirection: 'column' }}
+                  as="form"
+                >
+                  <Input
+                    onChange={e => setPrice(e.currentTarget.value)}
+                    placeholder="Token Price in ETH"
+                  />
+                </Flex>
+                <Flex mt={2}>
+                  <Button sx={{ bg: 'green' }} onClick={onSaleClick} variant="quartiary">
+                    Confirm
+                  </Button>
+                  <Button
+                    sx={{ bg: 'red' }}
+                    ml={2}
+                    onClick={() => setOnSale(false)}
+                    variant="quartiary"
+                  >
+                    Cancel
+                  </Button>
+                </Flex>
+              </Box>
+            )}
+            {!transfer && !onSaleActive && (
+              <Flex sx={{ flexDirection: 'column', width: '100%', justifyContent: 'center' }}>
+                <Button onClick={() => setTransfer(!transfer)} variant="tertiary">
+                  Transfer
+                </Button>
+                {isOnSale ? (
+                  <Button
+                    mt={2}
+                    onClick={() =>
+                      onSale &&
+                      onSale({
+                        id: token.id,
+                        price: Web3.utils.fromWei(token.price),
+                        onSale: false,
+                      })
+                    }
+                    variant="tertiary"
+                  >
+                    Remove from Sale
+                  </Button>
+                ) : (
+                  <Button mt={2} onClick={() => setOnSale(!onSaleActive)} variant="tertiary">
+                    Put Token for Sale
+                  </Button>
+                )}
+              </Flex>
+            )}
+          </Flex>
+        )}
+        {onBuy && (
+          <Flex mt={3} sx={{ justifyContent: 'center', width: '100%' }}>
+            <Button
+              sx={{
+                opacity: !!user?.ownedTokens.find(a => a.id === token.id) ? 0.5 : 1,
+                pointerEvents: !!user?.ownedTokens.find(a => a.id === token.id)
+                  ? 'none'
+                  : 'visible',
+              }}
+              onClick={onBuyClick}
+              variant="quartiary"
+            >
+              Buy Token
+            </Button>
+          </Flex>
+        )}
       </Box>
-      {onTransfer && (
-        <Flex mt={3} sx={{ justifyContent: 'center' }}>
-          {transfer && (
-            <Box sx={{ width: '100%' }}>
-              <Flex
-                onSubmit={onTransferClick}
-                sx={{ width: '100%', flexDirection: 'column' }}
-                as="form"
-              >
-                <Input
-                  onChange={e => setAddress(e.currentTarget.value)}
-                  placeholder="ETH Address 0x0..."
-                />
-              </Flex>
-              <Flex mt={2}>
-                <Button sx={{ bg: 'green' }} onClick={onTransferClick} variant="quartiary">
-                  Confirm
-                </Button>
-                <Button
-                  sx={{ bg: 'red' }}
-                  ml={2}
-                  onClick={() => setTransfer(false)}
-                  variant="quartiary"
-                >
-                  Cancel
-                </Button>
-              </Flex>
-            </Box>
-          )}
-          {onSaleActive && (
-            <Box sx={{ width: '100%' }}>
-              <Flex
-                onSubmit={onTransferClick}
-                sx={{ width: '100%', flexDirection: 'column' }}
-                as="form"
-              >
-                <Input
-                  onChange={e => setPrice(e.currentTarget.value)}
-                  placeholder="Token Price in ETH"
-                />
-              </Flex>
-              <Flex mt={2}>
-                <Button sx={{ bg: 'green' }} onClick={onSaleClick} variant="quartiary">
-                  Confirm
-                </Button>
-                <Button
-                  sx={{ bg: 'red' }}
-                  ml={2}
-                  onClick={() => setOnSale(false)}
-                  variant="quartiary"
-                >
-                  Cancel
-                </Button>
-              </Flex>
-            </Box>
-          )}
-          {!transfer && !onSaleActive && (
-            <Flex sx={{ flexDirection: 'column', width: '100%', justifyContent: 'center' }}>
-              <Button onClick={() => setTransfer(!transfer)} variant="tertiary">
-                Transfer
-              </Button>
-              {isOnSale ? (
-                <Button
-                  mt={2}
-                  onClick={() =>
-                    onSale &&
-                    onSale({ id: token.id, price: Web3.utils.fromWei(token.price), onSale: false })
-                  }
-                  variant="tertiary"
-                >
-                  Remove from Sale
-                </Button>
-              ) : (
-                <Button mt={2} onClick={() => setOnSale(!onSaleActive)} variant="tertiary">
-                  Put Token for Sale
-                </Button>
-              )}
-            </Flex>
-          )}
-        </Flex>
-      )}
-      {onBuy && (
-        <Flex mt={3} sx={{ justifyContent: 'center', width: '100%' }}>
-          <Button
-            sx={{
-              bg: 'green',
-              opacity: !!user?.ownedTokens.find(a => a.id === token.id) ? 0.5 : 1,
-              pointerEvents: !!user?.ownedTokens.find(a => a.id === token.id) ? 'none' : 'visible',
-            }}
-            onClick={onBuyClick}
-            variant="quartiary"
-          >
-            Buy Token
-          </Button>
-        </Flex>
-      )}
     </Card>
   )
 }
