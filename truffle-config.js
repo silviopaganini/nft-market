2a3252969d01c2d7a3cd3183bec82347ec2a82b9
Silvio Paganini <silvio@fluuu.id>
2021-04-07 10:12:51 +0100

    adding version to project

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,6 @@
 require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -47,19 +41,19 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
-      provider: () =>
-        new HDWalletProvider({
-          mnemonic: process.env.KEY_MNEMONIC,
-          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-          addressIndex: 0,
-        }),
-      network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
+    // ropsten: {
+    //   provider: () =>
+    //     new HDWalletProvider({
+    //       mnemonic: process.env.KEY_MNEMONIC,
+    //       providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    //       addressIndex: 0,
+    //     }),
+    //   network_id: 3, // Ropsten's id
+    //   gas: 5500000, // Ropsten has a lower block limit than mainnet
+    //   confirmations: 2, // # of confs to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    // },
 
     rinkeby: {
       provider: function () {
