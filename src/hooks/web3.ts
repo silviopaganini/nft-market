1fc8b84a37ee50fb625c184b24be8c583fa276ee
Silvio Paganini <silvio@fluuu.id>
2021-04-07 13:55:31 +0100

    auto change network

--- src/hooks/web3.ts
+++ src/hooks/web3.ts
@@ -36,44 +36,46 @@ export function useEagerConnect() {
 // }
 
 export function useInactiveListener(suppress: boolean = false) {
-  const { active, error, activate } = useWeb3React()
+  const { active, error } = useWeb3React()
 
   useEffect(() => {
     const { ethereum } = window as any
 
-    if (ethereum && ethereum.on && !active && !error && !suppress) {
-      const handleConnect = () => {
-        console.log("!!!!! Handling 'connect' event")
-        activate(injected)
-      }
-      const handleChainChanged = (chainId: string | number) => {
-        console.log("11111 Handling 'chainChanged' event with payload", chainId)
-        activate(injected)
-      }
-      const handleAccountsChanged = (accounts: string[]) => {
-        console.log("11111 Handling 'accountsChanged' event with payload", accounts)
-        if (accounts.length > 0) {
-          activate(injected)
-        }
-      }
-      const handleNetworkChanged = (networkId: string | number) => {
-        console.log("11111 Handling 'networkChanged' event with payload", networkId)
-        activate(injected)
-      }
+    ethereum.autoRefreshOnNetworkChange = true
 
-      ethereum.on('connect', handleConnect)
-      ethereum.on('chainChanged', handleChainChanged)
-      ethereum.on('accountsChanged', handleAccountsChanged)
-      ethereum.on('networkChanged', handleNetworkChanged)
+    // if (ethereum && ethereum.on && !active && !error && !suppress) {
+    //   const handleConnect = () => {
+    //     console.log("!!!!! Handling 'connect' event")
+    //     activate(injected)
+    //   }
+    //   const handleChainChanged = (chainId: string | number) => {
+    //     console.log("11111 Handling 'chainChanged' event with payload", chainId)
+    //     activate(injected)
+    //   }
+    //   const handleAccountsChanged = (accounts: string[]) => {
+    //     console.log("11111 Handling 'accountsChanged' event with payload", accounts)
+    //     if (accounts.length > 0) {
+    //       activate(injected)
+    //     }
+    //   }
+    //   const handleNetworkChanged = (networkId: string | number) => {
+    //     console.log("11111 Handling 'networkChanged' event with payload", networkId)
+    //     activate(injected)
+    //   }
 
-      return () => {
-        if (ethereum.removeListener) {
-          ethereum.removeListener('connect', handleConnect)
-          ethereum.removeListener('chainChanged', handleChainChanged)
-          ethereum.removeListener('accountsChanged', handleAccountsChanged)
-          ethereum.removeListener('networkChanged', handleNetworkChanged)
-        }
-      }
-    }
-  }, [active, error, suppress, activate])
+    //   ethereum.on('connect', handleConnect)
+    //   ethereum.on('chainChanged', handleChainChanged)
+    //   ethereum.on('accountsChanged', handleAccountsChanged)
+    //   ethereum.on('networkChanged', handleNetworkChanged)
+
+    //   return () => {
+    //     if (ethereum.removeListener) {
+    //       ethereum.removeListener('connect', handleConnect)
+    //       ethereum.removeListener('chainChanged', handleChainChanged)
+    //       ethereum.removeListener('accountsChanged', handleAccountsChanged)
+    //       ethereum.removeListener('networkChanged', handleNetworkChanged)
+    //     }
+    //   }
+    // }
+  }, [active, error, suppress])
 }
